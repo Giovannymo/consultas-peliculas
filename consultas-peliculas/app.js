e9a0c22c47c16cb002fa52fd9ce0fe16a7c55622
Giovanny Moreno <44243879+Giovannymo@users.noreply.github.com>
2023-06-05 21:25:59 -0500

    Adelanto review (solo falta consulta)

---
+++ consultas-peliculas/app.js
@@ -0,0 +1,84 @@
+const $table = document.getElementById("listMovies");
+const $tableBody = document.getElementById("tableBody")
+
+const $btnEnviar = document.getElementById("btnEnviar");
+const $btnRemove = document.querySelector('.remove')
+const $btnSearch = document.getElementById('btnSearch')
+const $inputSearch = document.getElementById('title')
+
+const $form = document.getElementById("formMovies");
+
+const $title = document.getElementById("title")
+const $gender = document.getElementById("gender")
+const $duration = document.getElementById("duration")
+const $director = document.getElementById("director")
+
+const movies = new Map();
+let count = 0;
+
+$form.addEventListener('click', enviar);
+$table.addEventListener('click', remove);
+$btnSearch.addEventListener('click', searcher)
+
+
+
+
+function enviar(e){
+    e.preventDefault();
+    const btnSelect = e.target;
+
+    if(btnSelect.id === 'btnEnviar'){
+        addMovie();
+    }
+
+}
+
+function addMovie(){
+    count = count+1;
+        movies.set('id', count)
+        movies.set('title',$title.value);
+        movies.set('gender',$gender.value);
+        movies.set('duration',$duration.value);
+        movies.set('director',$director.value);
+        showTable();
+}
+
+function searcher(e){
+
+    
+    console.log($inputSearch);
+}
+
+
+function remove(e){
+    e.preventDefault();
+    const btnSelect = e.target.classList[2];
+    if(btnSelect === 'remove'){
+        $tableBody.removeChild(e.target.parentNode)
+    }
+}
+
+function showTable(){
+    const rowMovie = document.createElement('tr')
+    const idMovie = document.createElement('th')
+    idMovie.setAttribute('scope', 'row')
+    idMovie.textContent = movies.get('id')
+    rowMovie.appendChild(idMovie)
+
+    for(const [key, value] of movies){
+        if(key !== 'id'){
+            const columnMovie = document.createElement('td')
+            columnMovie.textContent = value
+            columnMovie.setAttribute("class", "p-2" )
+            rowMovie.appendChild(columnMovie)
+        }
+    }
+    const btnRemove = document.createElement('button')
+    btnRemove.textContent= "Eliminar"
+    btnRemove.setAttribute("class", "btn btn-danger remove")
+    rowMovie.appendChild(btnRemove)
+
+    $tableBody.appendChild(rowMovie)    
+
+
+}
